69ab07ba85be2a5f3e19275a56a224aea04cec51
gvmotta <gustavomottadeveloper@gmail.com>
2024-09-23 23:38:31 -0300

    feat: starting project

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+  "extends": "next",
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
+};
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  base: './',
+  plugins: [react(), viteTsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/testing/setup-tests.ts',
+    exclude: ['**/node_modules/**', '**/e2e/**'],
+  },
+});

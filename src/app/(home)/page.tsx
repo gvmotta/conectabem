f27ba9fd337f3b4d06783536b23e80d16dbee452
gvmotta <gustavomottadeveloper@gmail.com>
2024-10-03 19:36:36 -0300

    testing

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,7 @@
 export default function HomePage() {
   return (
     <h1 className="text-red-600 text-xl">
-      Verificar se precisa está logado para acessar esta pagina (Pagina
-      principal)
+      Teste
     </h1>
   );
 }

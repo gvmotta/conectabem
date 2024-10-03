76626da70d1472765e354ffd70344dc1b0fcb24c
gvmotta <gustavomottadeveloper@gmail.com>
2024-10-03 19:33:16 -0300

    resolved: build

--- mock-server.ts
+++ mock-server.ts
@@ -3,9 +3,6 @@ import cors from 'cors';
 import express from 'express';
 import logger from 'pino-http';
 
-import { initializeDb } from './src/testing/mocks/db';
-import { handlers } from './src/testing/mocks/handlers';
-
 const app = express();
 
 app.use(
@@ -15,15 +12,15 @@ app.use(
   }),
 );
 
-app.use(express.json());
-app.use(logger({ level: 'silent' }));
-app.use(createMiddleware(...handlers));
+// app.use(express.json());
+// app.use(logger({ level: 'silent' }));
+// app.use(createMiddleware(...handlers));
 
-initializeDb().then(() => {
-  console.log('Mock DB initialized');
-  app.listen(process.env.NEXT_PUBLIC_MOCK_API_PORT, () => {
-    console.log(
-      `Mock API server started at http://localhost:${process.env.NEXT_PUBLIC_MOCK_API_PORT}`,
-    );
-  });
-});
+// initializeDb().then(() => {
+//   console.log('Mock DB initialized');
+//   app.listen(process.env.NEXT_PUBLIC_MOCK_API_PORT, () => {
+//     console.log(
+//       `Mock API server started at http://localhost:${process.env.NEXT_PUBLIC_MOCK_API_PORT}`,
+//     );
+//   });
+// });
